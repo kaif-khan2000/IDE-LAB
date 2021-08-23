7220acc5fbc8c3129dac7d09e39641e6b9f0e1bd
kaif <pkaif0667@gmail.com>
2021-08-23 17:03:42 +0530

    GUI based calculator

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+print("Addition = ",str(a+b),"\n")
+print("Multiplication = ",str(a*b),"\n")
+print("Substraction = ",str(a-b),"\n")
+print("Division = ",str(a/b),"\n")
+print("Modulo = ",str(a%b),"\n")
