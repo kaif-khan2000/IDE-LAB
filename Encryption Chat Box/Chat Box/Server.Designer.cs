d64c2f4966a22202a9a04b419749e3d76825352b
kaif <pkaif0667@gmail.com>
2021-11-03 10:37:01 +0530

    Encrption Decryption project completed

---
+++ Encryption Chat Box/Chat Box/Server.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Chat_Box
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayBox = new System.Windows.Forms.RichTextBox();
+            this.msgBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayBox
+            // 
+            this.displayBox.Location = new System.Drawing.Point(100, 46);
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(433, 257);
+            this.displayBox.TabIndex = 0;
+            this.displayBox.Text = "";
+            // 
+            // msgBox
+            // 
+            this.msgBox.Location = new System.Drawing.Point(100, 319);
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(329, 39);
+            this.msgBox.TabIndex = 1;
+            this.msgBox.Text = "";
+            this.msgBox.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Type your message";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.msgBox);
+            this.Controls.Add(this.displayBox);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox displayBox;
+        private System.Windows.Forms.RichTextBox msgBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Encryption Chat Box/Chat Box/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using SimpleTcp;
+namespace Chat_Box
+{
+    public partial class Server : Form
+    {
+        
+        public Server()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+        SimpleTcpServer server;
+        static string clientAdress = "";
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void Server_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer("127.0.0.1", 9000);
+            server.Events.ClientConnected += ClientConnected;
+            server.Events.ClientDisconnected += ClientDisconnected;
+            server.Events.DataReceived += DataReceived;
+            server.Start();
+        }
+
+        void ClientConnected(object sender, ClientConnectedEventArgs e) {
+            clientAdress = e.IpPort;
+            Console.WriteLine("client connected");
+        }
+
+        void ClientDisconnected(object sender, ClientDisconnectedEventArgs e) {
+            Console.WriteLine("Client disconnected");
+        }
+
+        void DataReceived(object sender, DataReceivedEventArgs e) {
+            displayBox.Invoke((MethodInvoker)(() =>
+            {
+                displayBox.Text += ("Client: " + Encoding.UTF8.GetString(e.Data) + "\n ");
+            }));    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String msg = msgBox.Text;
+            server.Send(clientAdress, msg);
+            msgBox.Text = "";
+            displayBox.Text += ("Server: " + msg + "\n");
+        }
+    }
+}
